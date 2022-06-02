5302e00a41c43d4463b570fd9b744e87a57de9bb
陈文豪 <chenwenhao.0401@bytedance.com>
2022-06-02 11:52:32 +0800

    first commit test

---
+++ QuickStart/src/test/java/com/bytedance/cg/bsm/quickstart/QuickStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytedance.cg.bsm.quickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
